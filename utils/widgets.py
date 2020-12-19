208071057e9680a769faec19bece76be163dbb87
Tushar <iit2019043@iiita.ac.in>
2020-12-19 23:00:29 +0530

    On the edge of completion

--- utils/widgets.py
+++ utils/widgets.py
@@ -1,8 +1,11 @@
+#importing libraries
+
 import pygame
 pygame.init()
 from utils.setandvar import*
 from utils.sprites import*
 
+#this function writes the text on the screen
 def writeonscreen(text,x,y, size):
     font = pygame.font.match_font('arial')
     fonter = pygame.font.Font(font, size)
@@ -10,7 +13,8 @@ def writeonscreen(text,x,y, size):
     text_rect = makesurface.get_rect()
     text_rect.center = (x, y)
     win.blit(makesurface, text_rect)
-    
+
+#this function displays the health of the player on the screen
 def displayhealth(health):
     ret = False
     if health <= 0:
@@ -26,10 +30,12 @@ def displayhealth(health):
     pygame.draw.rect(win, WHITE, outline, 2)
     return ret
 
+#this function displays life of the player
 def displaylives(life):
     for i in range (life):
         win.blit(playermini, (350 + (i-1)*tilesize, 15))
 
+#pausing the screen
 def pausethescreen():
     wait = True
     while wait:
@@ -42,6 +48,7 @@ def pausethescreen():
                     wait = False
 
 
+#printing the time elapsed for the powerups
 def print_time_elapsed(time_elapsed, text,y, z, x):
     length = 100
     height = 10
